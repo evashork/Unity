a21b961bd0659978ff7861da3be251be95b653eb
evashork <TACO@Kimui-MacBook-Air.local>
2015-10-20 17:42:30 +0900

    1

---
+++ spin.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spin : MonoBehaviour {
+	
+	public float rotateSpeed = 100f;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (0, rotateSpeed * Time.deltaTime, 0);
+		}
+		
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate (0, -rotateSpeed * Time.deltaTime, 0);
+		} 
+
+		Debug.Log (transform.rotation.y);
+		Debug.Log (transform.eulerAngles.y);
+	}
+}
