6416bcdcda329d4caca4e44af1e13338f2e8e701
evashork <TACO@Kimui-MacBook-Air.local>
2015-10-27 14:33:37 +0900

    1

---
+++ find.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointMGscript : MonoBehaviour {
+
+	public int point;
+	
+	// PointMG 오브젝트의 컴포넌트인 PointMGscript 스크립트 속 퍼블릭 함수에 접근
+
+	void Update () {
+
+		GameObject.Find("PointMG").GetComponent<PointMGscript>().UpdatePoint(); 
+
+	}
+}
