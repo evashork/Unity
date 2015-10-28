ef33cecd2f79b56b9e2e50d1fd36ccffc6ade2e6
evashork <evashork@naver.com>
2015-10-28 23:41:26 +0900

    11

---
+++ test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    public GameObject miniCube;
+    public GameObject miniPoint;
+    Vector3 shot;
+
+    void Update() {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            Invoke("Tac",1.5f);
+        }
+    }
+
+    void Tac() {
+      GameObject instantItem =  (GameObject)Instantiate(miniCube, miniPoint.transform.position, miniPoint.transform.rotation);
+        Rigidbody cubeRig = instantItem.GetComponent<Rigidbody>();
+        cubeRig.AddForce(shot * 100f, ForceMode.Impulse);
+    }
+}
