204886ad982134993e87c2eef8497f78c95a21aa
evashork <evashork@naver.com>
2016-03-21 10:40:42 +0900

    Update 상태저장하기.cs

--- 상태저장하기.cs
+++ 상태저장하기.cs
@@ -1,5 +1,4 @@
 // 데이터 저장하기
-
 void SaveS(){
   PlayerPrefs.SetInt("키이름",값);
   PlayerPrefs.SetString("키이름", 값);
@@ -7,8 +6,16 @@ void SaveS(){
 
 
 // 데이터 불러오기
-
 void LoadL(){
   Num = PlayerPrefs.GetInt("키이름");
   Name = PlayerPrefs.GetString("키이름");
+}
+
+
+// 데이터가 있는지 확인하기
+void start(){
+  if(PlayerPrefs.GetInt("키이름") != null)   
+      LoadL();
+  else
+      ...
 }
