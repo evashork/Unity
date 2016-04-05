a6f63f9e56674ee1dc0fc989ba2732c686d1b73d
evashork <evashork@naver.com>
2016-04-05 14:09:08 +0900

    Update 지정한위치로이동.cs

--- 지정한위치로이동.cs
+++ 지정한위치로이동.cs
@@ -3,4 +3,9 @@ public void Died () {
     transform.localPosition = new Vector3(0 , 0,  0);
     // 기본포지션
     transform.position = new Vector3(0 , 0,  0);
+    
+    // position의 경우 바로 값을 대입 할 수 없기 때문에 vector를 만들어서 값을 대입 한다
+    Vector3 temp = transform.position;
+    temp.y = 0;
+    transform.position = temp;
 }
