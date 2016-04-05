2044d0222c3aa4f73d5016c7a431f68c50a7ce5c
evashork <evashork@naver.com>
2016-04-05 14:03:46 +0900

    Update 지정한위치로이동.cs

--- 지정한위치로이동.cs
+++ 지정한위치로이동.cs
@@ -2,5 +2,5 @@ public void Died () {
     // 로컬포지션
     transform.localPosition = new Vector3(0 , 0,  0);
     // 기본포지션
-    transform.localPosition = new Vector3(0 , 0,  0);
+    transform.position = new Vector3(0 , 0,  0);
 }
