3085ef36b8db74de14280b0d6eaf21b31ef8bef4
evashork <evashork@naver.com>
2016-04-28 13:27:25 +0900

    Update 세이브및로드하기.cs

--- 세이브및로드하기.cs
+++ 세이브및로드하기.cs
@@ -7,15 +7,20 @@ public class save : MonoBehaviour {
 	string moneyType;
 
 	void Save(){
-
 		PlayerPrefs.SetInt ("money",moneyNum);  // "키값",데이터
 		PlayerPrefs.SetString ("moneyType",moneyType);  // "키값",데이터
-
 	}
 
 	void Load(){
-
 		moneyNum = PlayerPrefs.GetInt ("money");
 		moneyType = PlayerPrefs.GetString ("moneyType");
+	}
+
+	// 저장 데이터가 있는지 체크
+	void LoadCheck(){
+		if (PlayerPrefs.GetInt ("money") != null)
+			Load ();
+		else
+			Save ();
 	}
 }
