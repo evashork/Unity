67f09a11512973b4120f05653f2223a0caee9298
evashork <evashork@naver.com>
2017-09-26 10:23:28 +0900

    Update courutin.cs

--- courutin.cs
+++ courutin.cs
@@ -10,18 +10,32 @@ public class mgmissile : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		StartCoroutine (MgMissile ());
+		// 코루틴 시작하기
+		StartCoroutine ("MgMissile" , missileTime);
+		// 코루틴 멈추기
+		StopCoroutine ("MgMissile");
 	}
 
-	IEnumerator MgMissile(){
+	IEnumerator MgMissile(float missTime){
 		MakeMissile ();
 
-		yield return new WaitForSeconds (missileTime);
+		yield return new WaitForSeconds (missTime);
 
-		StartCoroutine (MgMissile ());
+		StartCoroutine ("MgMissile");
 	}
 
 	void MakeMissile(){
 		Instantiate (missile, missilePoint.transform.position, missilePoint.transform.rotation);
 	}
+	
+	IEnumerator FadeIn(){
+		while(true){
+			yield return new WaitForSeconds (2f);
+			print ("co!!");
+			if (gs.GetComponent<GS> ().GT == GameState.Start) {
+				break;
+			}
+		}
+	}
+	
 }
