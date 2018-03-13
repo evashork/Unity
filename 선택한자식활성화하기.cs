55f416e622d2e840edacc04e74d0d64d84e5a518
evashork <evashork@naver.com>
2018-03-13 11:36:59 +0900

    Create 선택한자식활성화하기.cs

---
+++ 선택한자식활성화하기.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shopGS : MonoBehaviour {
+
+    public Transform mario;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray.origin, ray.direction, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    SelectChr(hit.collider.name);
+                }
+            }
+        }
+    }
+
+    void SelectChr(string chrName)
+    {
+        for (int i = 0; i < mario.childCount; i++){
+            if(chrName == mario.transform.GetChild(i).name) {
+                mario.transform.GetChild(i).gameObject.SetActive(true);
+            }else{
+                mario.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+}
